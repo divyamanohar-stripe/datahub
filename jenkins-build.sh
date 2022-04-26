64f466b16e75657f7b0abd0aaf77c155a141688d
Piyush Narang <piyushn@stripe.com>
2022-04-26 16:45:26 -0400

    Add metadata service artifacts

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -36,8 +36,15 @@ echo Building datahub.
 # We just compile the metadata service and frontend for now
 ./gradlew :metadata-service:war:build
 
-# TODO: reenable tests 
-./gradlew :datahub-frontend:dist -x test -x yarnTest -x yarnLint
+./gradlew :datahub-frontend:dist
+
+# Metadata service also requires some jetty related jars
+curl https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-runner/9.4.20.v20190813/jetty-runner-9.4.20.v20190813.jar --output /build/jetty-runner.jar
+curl https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-jmx/9.4.20.v20190813/jetty-jmx-9.4.20.v20190813.jar --output /build/jetty-jmx.jar
+curl https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-util/9.4.20.v20190813/jetty-util-9.4.20.v20190813.jar --output /build/jetty-util.jar
 
 cp datahub-frontend/build/distributions/datahub-frontend.zip /build/datahub-frontend.zip
-# todo as part of setting up the container builds, copy artifacts
+cp metadata-service/war/build/libs/war.war /build/war.war
+cp metadata-models/src/main/resources/entity-registry.yml /build/entity-registry.yml
+cp docker/datahub-gms/jetty.xml /build/jetty.xml
+# TODO copy a metadata startup script!
