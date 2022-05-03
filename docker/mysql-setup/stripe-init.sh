fca590afc4d9e2af7a96006470bbe61165de58dd
Piyush Narang <piyushn@stripe.com>
2022-05-03 14:16:21 -0400

    More mysql init fixes

--- docker/mysql-setup/stripe-init.sh
+++ docker/mysql-setup/stripe-init.sh
@@ -3,5 +3,5 @@
 # This secret must be configured in the datahub-gms sky cfg file
 MYSQL_PASSWORD=$(cat /pay/keys/db_root_password.txt)
 
-sed -e "s/DATAHUB_DB_NAME/${DATAHUB_DB_NAME}/g" init.sql | tee -a /tmp/init-final.sql
+sed -e "s/DATAHUB_DB_NAME/${DATAHUB_DB_NAME}/g" /datahub/datahub-gms/scripts/init.sql | tee -a /tmp/init-final.sql
 mysql -u $MYSQL_USERNAME -p"$MYSQL_PASSWORD" -h $MYSQL_HOST < /tmp/init-final.sql
