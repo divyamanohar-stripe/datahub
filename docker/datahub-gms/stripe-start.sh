af0025096151a6cbdebf89eb7d098ea3e933a38e
Piyush Narang <piyushn@stripe.com>
2022-05-03 15:27:55 -0400

    Log env details

--- docker/datahub-gms/stripe-start.sh
+++ docker/datahub-gms/stripe-start.sh
@@ -5,6 +5,8 @@ set -x
 EBEAN_DATASOURCE_PASSWORD=$(cat /pay/keys/db_root_password.txt)
 export EBEAN_DATASOURCE_PASSWORD
 
+echo "Starting DataHub GMS with: $ENTITY_REGISTRY_CONFIG_PATH"
+
 # Stripe specific tweaks to `start.sh` so that we can get it to work in our setup. The existing start.sh
 # issues a set of wait calls and uses the dockerize command to kick things off. We just wrap the java launch here
 exec java $JAVA_OPTS $JMX_OPTS \
