cd6100494f0da175561953bee3f9ff6bd137b707
Divya Manohar <divyamanohar@stripe.com>
2022-09-09 14:24:20 -0500

    historical sla tracking improvements (#102)
    ## Notify
    cc stripe-private-oss-forks/schema-and-query
    r? szhou || piyushn
    
    ## Summary
    - Line up execution dates across charts
    - move owner to title and make charts smaller
    - show landing time instead of duration on chart tooltip
    - commented code
    
    ## Motivation
    https://docs.google.com/document/d/15UhFWbyIVSluWROTHfKKuluQBWClf3dVWqLaF6uHXko/edit
    https://docs.google.com/document/d/1Ey59tHXF0rsMNMhfICMAb_tWnSLqFzvZd4_pw6w-NnU/edit#
    
    ## Test plan
    tested locally:
    
    <img width="1402" alt="Screen Shot 2022-09-09 at 11 56 15 AM" src="https://git.corp.stripe.com/storage/user/4521/files/524fb259-3dc6-475a-a177-2a1bd5815c37">
    
    
    ## Rollback / Rollout
    safe to revert
    
    
    Service Labels
    ==============
    s: datahub-frontend
    s: datahub-gms
    (Squashed by Merge Queue - Original PR: https://git.corp.stripe.com/stripe-private-oss-forks/datahub/pull/102)

--- datahub-web-react/src/app/entity/domain/profile/HistoricalSLATracking.tsx
+++ datahub-web-react/src/app/entity/domain/profile/HistoricalSLATracking.tsx
@@ -38,7 +38,7 @@ type DataJobProperties = {
     [key: string]: string;
 };
 
-type RunCustomPropertiesWithExternalUrl = {
+type RunProperties = {
     executionDate: string;
     externalUrl: string;
     state: string;
@@ -46,13 +46,18 @@ type RunCustomPropertiesWithExternalUrl = {
     endDate: string;
 };
 
-type Run = RunCustomPropertiesWithExternalUrl & {
+type Run = RunProperties & {
     errorTimeLeftToEnd: number;
     runDuration: number;
     landingTime: number;
     color: string | null;
 };
 
+/**
+ * Convert seconds into human readable format
+ * @param seconds number of seconds to format
+ * @param showSeconds boolean to show seconds in display
+ */
 function convertSecsToHumanReadable(seconds: number, showSeconds: boolean) {
     const oriSeconds = seconds;
     const floatingPart = oriSeconds - Math.floor(oriSeconds);
@@ -86,13 +91,15 @@ function convertSecsToHumanReadable(seconds: number, showSeconds: boolean) {
     return readableFormat;
 }
 
-function formatDateString(date: string | undefined) {
-    if (date === undefined) {
-        return date;
-    }
+function formatDateString(date: string) {
     return moment.utc(date).format(DATE_DISPLAY_TOOLTIP_FORMAT);
 }
 
+/**
+ * Calculate percentile from array of values
+ * @param arr array of numeric values
+ * @param q percentile (ex. 0.9 => p90)
+ */
 const quantile = (arr, q) => {
     const sorted = arr.sort((a, b) => a - b);
     const pos = (sorted.length - 1) * q;
@@ -102,23 +109,50 @@ const quantile = (arr, q) => {
     return sorted[base];
 };
 
-function renderPlotHeader(taskId: string, finishedBySla: string) {
+/**
+ * Get all unique execution dates across all data jobs in order to line up timelines across charts
+ * @param dataJobQueryResults list of DataJob query results
+ */
+function getAllExecDates(dataJobQueryResults) {
+    const allExecDates = new Set();
+    dataJobQueryResults.forEach((dataJob) => {
+        const runs = dataJob?.dataJob?.runs?.runs?.map((run) => ({
+            ...run?.properties?.customProperties?.reduce((acc, e) => ({ ...acc, [e.key]: e.value }), {}),
+        }));
+        runs.map((r) => allExecDates.add(formatDateString(r?.executionDate)));
+    });
+    return Array.from(allExecDates).sort();
+}
+
+/**
+ * render header of chart with title: "Team: did task succees in SLA?"
+ * @param taskId taskId of DataJob
+ * @param finishedBySla error end SLA
+ * @param project team owner of DataJob
+ */
+function renderPlotHeader(taskId: string, finishedBySla: string, project: string) {
     const finishedBySlaHours = moment.duration(finishedBySla, 'seconds').asHours();
     return (
         <Descriptions
-            title={`Did ${taskId} succeed in ${finishedBySlaHours} hours?`}
+            title={`${project}: Did ${taskId} succeed in ${finishedBySlaHours} hours?`}
             bordered
             style={{ marginTop: '15px' }}
         />
     );
 }
 
-function renderSlaMissSummary(runs: Run[], project) {
+/**
+ * Create side SLA miss summary with % met deadline and p90 landing time
+ * @param runs list of DataJob run results
+ */
+function renderSlaMissSummary(runs: Run[]) {
+    // return color of met deadline tag based on percentage of SLA meets
     function getTagColor(metDeadlinePercentage: number) {
         if (metDeadlinePercentage < 70) return 'red';
         if (metDeadlinePercentage < 100) return 'yellow';
         return 'blue';
     }
+
     const metDeadlinePercentage = (
         (runs.filter((r) => {
             return r.errorTimeLeftToEnd >= 0;
@@ -126,38 +160,68 @@ function renderSlaMissSummary(runs: Run[], project) {
             runs.length) *
         100.0
     ).toFixed(2);
+
     const landingTimes = runs.map((r) => r.landingTime);
     const p90Landing = convertSecsToHumanReadable(quantile(landingTimes, 0.9), false);
     const tagColor = getTagColor(parseInt(metDeadlinePercentage, 10));
+
     return (
-        <Descriptions title="" bordered size="small" column={{ md: 1 }} style={{ marginLeft: '20px' }}>
+        <Descriptions title="" bordered size="small" column={{ md: 1 }} style={{ marginLeft: '20px', height: '2px' }}>
             <Descriptions.Item style={{ fontWeight: 'bold' }} label="Met Deadline">
                 <Tag color={tagColor}>{`${metDeadlinePercentage}%`}</Tag>
             </Descriptions.Item>
             <Descriptions.Item style={{ fontWeight: 'bold' }} label="p90 Delivery">
                 {`${p90Landing}`}
             </Descriptions.Item>
-            <Descriptions.Item style={{ fontWeight: 'bold' }} label="Task Owner">
-                {`${project}`}
-            </Descriptions.Item>
         </Descriptions>
     );
 }
 
-function renderTimelinessPlot(runs: Run[], domainName) {
+/**
+ * Create columm chart displaying hours until SLA is missed
+ * @param runs list of DataJob run results
+ * @param domainName name of domain on DataJob for linking to domain timeliness page
+ * @param allExecDates list of unique execution dates to line up x-axis across charts
+ */
+function renderTimelinessPlot(runs: Run[], domainName, allExecDates: string[]) {
+    // create mapping of execution date to color of column based whether missed SLA or not
     const slaMissData = new Map();
-    const runsPlotData = runs.map((r) => {
+    let runsPlotData = runs.map((r) => {
         if (r.errorTimeLeftToEnd > 0) {
             slaMissData.set(formatDateString(r.executionDate), 'CornflowerBlue');
         } else {
             slaMissData.set(formatDateString(r.executionDate), 'Crimson');
         }
-
         return {
             ...r,
             execDate: formatDateString(r.executionDate),
             values: r.errorTimeLeftToEnd,
         };
+    });
+
+    // get missing dates from current plot, add them in with null run values, and sort by exec date
+    const currPlotDataExecDates = runsPlotData.map((r) => r.execDate);
+    const missingDates = allExecDates.filter((x) => !currPlotDataExecDates.includes(x));
+    missingDates.forEach((date) => {
+        const nullRun = {
+            errorTimeLeftToEnd: 0,
+            runDuration: 0,
+            landingTime: 0,
+            color: null,
+            executionDate: 'none',
+            externalUrl: 'none',
+            state: 'none',
+            startDate: 'none',
+            endDate: 'none',
+            execDate: date,
+            values: 0,
+        };
+
+        runsPlotData.push(nullRun);
+    });
+    runsPlotData = runsPlotData.sort(function (a, b) {
+        if (a.execDate > b.execDate) return 1;
+        return -1;
     });
 
     const config: ComponentProps<typeof Column> = {
@@ -173,6 +237,7 @@ function renderTimelinessPlot(runs: Run[], domainName) {
             label: {
                 formatter: (strDate) => {
                     const date = moment.utc(strDate);
+                    // if UTC midnight run, remove hours/minutes from exec date label
                     if (date.hour() === 0 && date.minute() === 0) {
                         return date.format(DATE_DAILY_DISPLAY_FORMAT);
                     }
@@ -184,7 +249,11 @@ function renderTimelinessPlot(runs: Run[], domainName) {
             title: {
                 text: 'Time Until SLA',
             },
-            label: { formatter: (val) => `${(+val / 3600).toFixed(2)}h` },
+            label: {
+                formatter: (val) =>
+                    // display y-axis in decimal hours
+                    `${(+val / 3600).toFixed(2)}h`,
+            },
         },
         color: (execDate) => {
             return slaMissData.get(execDate.execDate);
@@ -192,6 +261,7 @@ function renderTimelinessPlot(runs: Run[], domainName) {
         onEvent: (chart, event) => {
             if (event.type === 'plot:click') {
                 if (domainName === 'UAR') {
+                    // link to domain timeliness page based on domain name and execution date
                     const anchor = document.createElement('a');
                     const currExecDate = event.data?.data?.executionDate;
                     const currMoment = moment.utc(currExecDate).format(DATE_SEARCH_PARAM_FORMAT);
@@ -207,39 +277,50 @@ function renderTimelinessPlot(runs: Run[], domainName) {
             enterable: true,
             domStyles: {
                 'g2-tooltip': {
-                    width: '315px',
+                    width: '275px',
                     padding: 0,
                 },
             },
             customContent: (title, items) => {
                 const run = items[0]?.data as Run;
-                const status =
-                    run?.errorTimeLeftToEnd > 0
-                        ? `<div>${run?.state} with ${convertSecsToHumanReadable(
-                              run?.errorTimeLeftToEnd,
-                              true,
-                          )} left to spare</div>`
-                        : `<div>${run?.state}, but ${convertSecsToHumanReadable(
-                              run?.errorTimeLeftToEnd * -1,
-                              true,
-                          )} late</div>`;
-                const execDate = `<div>Execution date: ${formatDateString(run?.executionDate)}</div>`;
-                const startDate = `<div>Start date: ${formatDateString(run?.startDate)}</div>`;
-                const endDate = `<div>End Date: ${formatDateString(run?.endDate)}</div>`;
-                const duration = `<div>Duration: ${convertSecsToHumanReadable(run?.runDuration, true)}</div>`;
-                return `<div>${status}${execDate}${startDate}${endDate}${duration}</div>`;
+                if (run?.executionDate !== 'none') {
+                    const status =
+                        run?.errorTimeLeftToEnd > 0
+                            ? `<div>${run?.state} with ${convertSecsToHumanReadable(
+                                  run?.errorTimeLeftToEnd,
+                                  true,
+                              )} left to spare</div>`
+                            : `<div>${run?.state}, but ${convertSecsToHumanReadable(
+                                  run?.errorTimeLeftToEnd * -1,
+                                  true,
+                              )} late</div>`;
+                    const execDate = `<div>Execution date: ${formatDateString(run?.executionDate)}</div>`;
+                    const startDate = `<div>Start date: ${formatDateString(run?.startDate)}</div>`;
+                    const endDate = `<div>End Date: ${formatDateString(run?.endDate)}</div>`;
+                    const landingTime = `<div>Landing Time: T+${convertSecsToHumanReadable(
+                        run?.landingTime,
+                        true,
+                    )}</div>`;
+                    return `<div>${status}${execDate}${startDate}${endDate}${landingTime}</div>`;
+                }
+                return '';
             },
         },
     };
 
     return (
         <>
-            <Column {...config} style={{ marginLeft: '20px', height: '200px' }} />
+            <Column {...config} style={{ marginLeft: '20px', height: '130px' }} />
         </>
     );
 }
 
-function formatDataAndRenderPlots(dataJob) {
+/**
+ * Create DataJobProperties & RunProperties from dataJob query result and render layout
+ * @param dataJob DataJob query result
+ * @param allExecDates list of unique execution dates to line up timeliness x-axis across charts
+ */
+function formatDataAndRenderPlots(dataJob, allExecDates) {
     const now = moment.utc();
     const dataJobProperties = dataJob?.dataJob?.properties?.customProperties?.reduce(
         (acc, e) => ({ ...acc, [e.key]: e.value }),
@@ -256,7 +337,7 @@ function formatDataAndRenderPlots(dataJob) {
                 ({
                     ...run?.properties?.customProperties?.reduce((acc, e) => ({ ...acc, [e.key]: e.value }), {}),
                     externalUrl: run?.externalUrl,
-                } as RunCustomPropertiesWithExternalUrl),
+                } as RunProperties),
         )
         .map((r) => {
             const endDate: moment.Moment = r.endDate === 'None' ? now : moment(r.endDate);
@@ -286,10 +367,16 @@ function formatDataAndRenderPlots(dataJob) {
     return (
         <>
             <Layout>
-                <Header>{renderPlotHeader(dataJobProperties.taskId, dataJobProperties.finishedBySla)}</Header>
+                <Header>
+                    {renderPlotHeader(
+                        dataJobProperties.taskId,
+                        dataJobProperties.finishedBySla,
+                        dataJobProperties.project,
+                    )}
+                </Header>
                 <Layout>
-                    <Sider>{renderSlaMissSummary(latestRuns, dataJobProperties.project)}</Sider>
-                    <Content>{renderTimelinessPlot(latestRuns, dataJobProperties.domainName)}</Content>
+                    <Sider>{renderSlaMissSummary(latestRuns)}</Sider>
+                    <Content>{renderTimelinessPlot(latestRuns, dataJobProperties.domainName, allExecDates)}</Content>
                 </Layout>
             </Layout>
         </>
@@ -343,13 +430,15 @@ export const HistoricalSLATracking = () => {
         return loadingPage;
     }
 
+    const uniqueExecDates = getAllExecDates([dataJobUAR, dataJobRecon, dataJobCost, dataJobBook, dataJobGTM]);
+
     return (
         <>
-            {formatDataAndRenderPlots(dataJobUAR)}
-            {formatDataAndRenderPlots(dataJobRecon)}
-            {formatDataAndRenderPlots(dataJobCost)}
-            {formatDataAndRenderPlots(dataJobBook)}
-            {formatDataAndRenderPlots(dataJobGTM)}
+            {formatDataAndRenderPlots(dataJobUAR, uniqueExecDates)}
+            {formatDataAndRenderPlots(dataJobRecon, uniqueExecDates)}
+            {formatDataAndRenderPlots(dataJobCost, uniqueExecDates)}
+            {formatDataAndRenderPlots(dataJobBook, uniqueExecDates)}
+            {formatDataAndRenderPlots(dataJobGTM, uniqueExecDates)}
         </>
     );
 };
