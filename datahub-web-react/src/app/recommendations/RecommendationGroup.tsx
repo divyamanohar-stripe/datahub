c21d029ce2fb5d3ef18b138f1f1262fc8ab576c6
Matthew Sewall <mattsewall@stripe.com>
2022-12-19 09:19:02 -0800

    Further Homepage Tweaks to Remove Overlapping Text (#248)
    ### Notify
    cc stripe-private-oss-forks/data-governance
    r? szhou
    
    
    ### Summary
    Unfortunate that local environment doesn't render top viewed recommendations and I'm slight testing off of prod
    
    ### Motivation
    Remove overlapping text from homepage
    
    ### Test plan
    
    
    ### Rollout/monitoring/revert plan
    Safe to revert unless specified otherwise
    
    
    Service Labels
    ==============
    s: datahub-frontend
    s: datahub-gms
    (Squashed by Merge Queue - Original PR: https://git.corp.stripe.com/stripe-private-oss-forks/datahub/pull/248)

--- datahub-web-react/src/app/recommendations/RecommendationGroup.tsx
+++ datahub-web-react/src/app/recommendations/RecommendationGroup.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useMemo } from 'react';
 import { Col, Divider, Row, Typography } from 'antd';
 import styled from 'styled-components';
 import { RecommendationModule as RecommendationModuleType, ScenarioType } from '../../types.generated';
@@ -12,6 +12,32 @@ export const RecommendationsContainer = styled.div`
 
 export const RecommendationContainer = styled.div`
     margin-bottom: 32px;
+    max-width: 1000px;
+    min-width: 750px;
+`;
+
+export const RecommendationContainerHalf = styled.div`
+    margin-bottom: 32px;
+    max-width: 500px;
+    min-width: 375px;
+    padding-left: 4px;
+    padding-right: 4px;
+`;
+
+export const RecommendationContainerThird = styled.div`
+    margin-bottom: 32px;
+    max-width: 333px;
+    min-width: 250px;
+    padding-left: 3px;
+    padding-right: 3px;
+`;
+
+export const RecommendationContainerFourth = styled.div`
+    margin-bottom: 32px;
+    max-width: 250px;
+    min-width: 185px;
+    padding-left: 2px;
+    padding-right: 2px;
 `;
 
 export const RecommendationTitle = styled(Typography.Title)`
@@ -30,28 +56,45 @@ type Props = {
 };
 
 export const RecommendationGroup = ({ modules }: Props) => {
-    const getColumnWidth = useCallback(() => {
+    const columnWidth = useMemo(() => {
         const modulesLength = modules.length;
         return 24 / modulesLength;
     }, [modules]);
-    const getColumns = useCallback(() => {
-        const recommendationRender = modules.map((module) => (
-            <RecommendationContainer key={module.title}>
-                <RecommendationTitle level={4}>{module.title}</RecommendationTitle>
-                <ThinDivider />
-                <RecommendationModule
-                    key={module.moduleId}
-                    module={module as RecommendationModuleType}
-                    scenarioType={ScenarioType.Home}
-                    showTitle={false}
-                />
-            </RecommendationContainer>
-        ));
+    const componentType = useMemo(() => {
+        switch (columnWidth) {
+            case 24:
+                return RecommendationContainer;
+            case 12:
+                return RecommendationContainerHalf;
+            case 8:
+                return RecommendationContainerThird;
+            case 6:
+                return RecommendationContainerFourth;
+            default:
+                return RecommendationContainer;
+        }
+    }, [columnWidth]);
+    const columns = useMemo(() => {
+        const recommendationRender = modules.map((module) => {
+            const SpecificComponent = componentType;
+            return (
+                <SpecificComponent key={module.title}>
+                    <RecommendationTitle level={4}>{module.title}</RecommendationTitle>
+                    <ThinDivider />
+                    <RecommendationModule
+                        key={module.moduleId}
+                        module={module as RecommendationModuleType}
+                        scenarioType={ScenarioType.Home}
+                        showTitle={false}
+                    />
+                </SpecificComponent>
+            );
+        });
         return recommendationRender.map((module) => (
-            <Col span={getColumnWidth()} key={module.key}>
+            <Col span={columnWidth} key={module.key}>
                 {module}
             </Col>
         ));
-    }, [modules, getColumnWidth]);
-    return <Row>{getColumns()}</Row>;
+    }, [modules, columnWidth, componentType]);
+    return <Row>{columns}</Row>;
 };
