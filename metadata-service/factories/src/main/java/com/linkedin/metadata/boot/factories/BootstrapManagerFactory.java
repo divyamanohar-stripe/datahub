b5f4ddcf449c1f039458058495cc4c92d0b7b786
Matthew Sewall <mattsewall@stripe.com>
2023-01-17 16:40:23 -0800

    Revert "fix(policies): Re-revert the policies fix + ingest documents directly…" (#278)
    Reverts stripe-private-oss-forks/datahub#277
    
    
    Service Labels
    ==============
    s: datahub-frontend
    s: datahub-gms
    (Squashed by Merge Queue - Original PR: https://git.corp.stripe.com/stripe-private-oss-forks/datahub/pull/278)

--- metadata-service/factories/src/main/java/com/linkedin/metadata/boot/factories/BootstrapManagerFactory.java
+++ metadata-service/factories/src/main/java/com/linkedin/metadata/boot/factories/BootstrapManagerFactory.java
@@ -2,9 +2,6 @@ package com.linkedin.metadata.boot.factories;
 
 import com.google.common.collect.ImmutableList;
 import com.linkedin.gms.factory.entity.EntityServiceFactory;
-import com.linkedin.gms.factory.entityregistry.EntityRegistryFactory;
-import com.linkedin.gms.factory.search.EntitySearchServiceFactory;
-import com.linkedin.gms.factory.search.SearchDocumentTransformerFactory;
 import com.linkedin.metadata.boot.BootstrapManager;
 import com.linkedin.metadata.boot.steps.IngestDataPlatformInstancesStep;
 import com.linkedin.metadata.boot.steps.IngestDataPlatformsStep;
@@ -12,9 +9,6 @@ import com.linkedin.metadata.boot.steps.IngestPoliciesStep;
 import com.linkedin.metadata.boot.steps.IngestRetentionPoliciesStep;
 import com.linkedin.metadata.boot.steps.IngestRootUserStep;
 import com.linkedin.metadata.entity.EntityService;
-import com.linkedin.metadata.models.registry.EntityRegistry;
-import com.linkedin.metadata.search.EntitySearchService;
-import com.linkedin.metadata.search.transformer.SearchDocumentTransformer;
 import io.ebean.EbeanServer;
 import javax.annotation.Nonnull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +20,7 @@ import org.springframework.context.annotation.Scope;
 
 
 @Configuration
-@Import({EntityServiceFactory.class, EntityRegistryFactory.class, EntitySearchServiceFactory.class,
-    SearchDocumentTransformerFactory.class})
+@Import({EntityServiceFactory.class})
 public class BootstrapManagerFactory {
 
   @Autowired
@@ -35,18 +28,6 @@ public class BootstrapManagerFactory {
   private EntityService _entityService;
 
   @Autowired
-  @Qualifier("entityRegistry")
-  private EntityRegistry _entityRegistry;
-
-  @Autowired
-  @Qualifier("entitySearchService")
-  private EntitySearchService _entitySearchService;
-
-  @Autowired
-  @Qualifier("searchDocumentTransformer")
-  private SearchDocumentTransformer _searchDocumentTransformer;
-
-  @Autowired(required = false)
   @Qualifier("ebeanServer")
   private EbeanServer _server;
 
@@ -59,8 +40,7 @@ public class BootstrapManagerFactory {
   @Nonnull
   protected BootstrapManager createInstance() {
     final IngestRootUserStep ingestRootUserStep = new IngestRootUserStep(_entityService);
-    final IngestPoliciesStep ingestPoliciesStep =
-        new IngestPoliciesStep(_entityRegistry, _entityService, _entitySearchService, _searchDocumentTransformer);
+    final IngestPoliciesStep ingestPoliciesStep = new IngestPoliciesStep(_entityService);
     final IngestDataPlatformsStep ingestDataPlatformsStep = new IngestDataPlatformsStep(_entityService);
     final IngestDataPlatformInstancesStep ingestDataPlatformInstancesStep =
         new IngestDataPlatformInstancesStep(_entityService, _server);
--- metadata-service/restli-client/src/main/java/com/linkedin/entity/client/JavaEntityClient.java
+++ metadata-service/restli-client/src/main/java/com/linkedin/entity/client/JavaEntityClient.java
@@ -249,7 +249,6 @@ public class JavaEntityClient implements EntityClient {
      *
      * @param input search query
      * @param filter search filters
-     * @param sortCriterion sort criterion
      * @param start start offset for search results
      * @param count max number of search results requested
      * @return Snapshot key
@@ -260,12 +259,11 @@ public class JavaEntityClient implements EntityClient {
         @Nonnull String entity,
         @Nonnull String input,
         @Nullable Filter filter,
-        @Nullable SortCriterion sortCriterion,
         int start,
         int count,
         @Nonnull final Authentication authentication)
         throws RemoteInvocationException {
-        return _entitySearchService.search(entity, input, filter, sortCriterion, start, count);
+        return _entitySearchService.search(entity, input, filter, null, start, count);
     }
 
     /**
